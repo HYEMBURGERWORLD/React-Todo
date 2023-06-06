8d67aa29e4cf1e17c7e9d097f462f81d376896c5
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-06-06 17:48:45 +0900

    🚩 Feat Todo templet

---
+++ src/components/TodoTemplet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function TodoTemplet({ children }) {
+  const TodoTempletStyle = styled.div`
+    width: 700px;
+    height: 1000px;
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  `;
+
+  return <TodoTempletStyle>{children}</TodoTempletStyle>;
+}
