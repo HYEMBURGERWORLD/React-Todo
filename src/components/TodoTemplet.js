3d0e35ad28dc2b6e61c51b72a9139f1e245639c8
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-06-06 21:05:31 +0900

    💄 Update style

--- src/components/TodoTemplet.js
+++ src/components/TodoTemplet.js
@@ -8,6 +8,8 @@ export default function TodoTemplet({ children }) {
     background-color: white;
     border-radius: 20px;
     box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+    display: flex;
+    flex-direction: column;
   `;
 
   return <TodoTempletStyle>{children}</TodoTempletStyle>;
